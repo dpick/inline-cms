c1481a62573c3055936848b247ad628b500066be
David Pick <pickdavid@gmail.com>
2011-10-02 17:20:14 -0500

    don't save updated text if user is not an admin

--- app/controllers/text_object_controller.rb
+++ app/controllers/text_object_controller.rb
@@ -1,9 +1,8 @@
 class TextObjectController < ApplicationController
   def save
+    return unless current_user.try(:admin?)
     text_object = TextObject.find_by_key(params[:id])
     text_object.update_attributes(:value => params[:value])
-
-    p text_object
 
     respond_to do |format|
       format.json { render :json => text_object.value }
--- spec/controllers/text_object_controller_spec.rb
+++ spec/controllers/text_object_controller_spec.rb
@@ -3,7 +3,9 @@ require 'spec_helper'
 describe TextObjectController do
   before :each do
     @text_object = TextObject.new
+    @user = mock('user', :admin? => true)
     TextObject.stub!(:find_by_key).and_return(@text_object)
+    controller.stub!(:current_user).and_return(@user)
   end
 
   it "should return the value of the text object" do
