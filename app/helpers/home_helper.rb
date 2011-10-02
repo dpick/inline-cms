d910723fad42e0d7e293320df8a52f2616993824
David Pick <pickdavid@gmail.com>
2011-10-01 22:57:08 -0500

    correct check if key is already in db

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,10 +1,9 @@
 module HomeHelper
   def text_for(key, default_value = "double click here to edit text")
     text_object = TextObject.find_or_create_by_key(key)
-    if text_object.new_record?
-      puts "it was a new record"
-      text_object.update_attributes(:value => default_value) 
-    end
+
+    text_object.update_attributes(:value => default_value) if text_object.value.nil?
+
     text_object.value
   end
 end
