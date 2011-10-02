e23d49c1af8075d5c3ece432f9e12806887b2e75
David Pick <pickdavid@gmail.com>
2011-10-01 22:51:56 -0500

    added helper method for getting the key value

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,10 @@
 module HomeHelper
+  def text_for(key, default_value = "double click here to edit text")
+    text_object = TextObject.find_or_create_by_key(key)
+    if text_object.new_record?
+      puts "it was a new record"
+      text_object.update_attributes(:value => default_value) 
+    end
+    text_object.value
+  end
 end
