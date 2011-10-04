776bde8ef5fd14533ea9416ae7b2fc47ce0787e6
Pete Brousalis <brousapg@gmail.com>
2011-10-03 23:28:56 -0500

    Some beautiful form for login

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,9 +1,14 @@
 $(document).ready(function() {
   $(".alert-message").alert();
   $(".topbar").dropdown();
+  $('#login input').attr('autocomplete','off');
+  $("input:visible:enabled:first").focus();
   $('.edit').editable('textobject/save', {
     indicator : 'Saving...',
     tooltip   : 'Click to edit...',
+    callback  : function(value, settings) {
+      console.log('here');
+    }
   });
 
   $('.edit_area').editable('textobject/save', { 
