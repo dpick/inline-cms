f3bd9a9118421ef9729bb124d09216481c43b899
David Pick <pickdavid@gmail.com>
2011-10-12 21:38:40 -0500

    make sure the images directory is there for heroku

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -2,6 +2,8 @@ class ImagesController < ApplicationController
   def upload_to_imgur
     path = File.join(Rails.root, "tmp/images/#{Time.now}.#{params[:file].original_filename.split(".").last}")
 
+    Dir.mkdir("tmp/images") #make sure images directory exists
+
     File.open(path, "wb") { |f| f.write(params['file'].tempfile.read) }
 
     @uploaded_img = $imgur.upload_file path
