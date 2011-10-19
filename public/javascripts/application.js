b4bcec836112fd7ad2e86f81c95b1a472c591c72
David Pick <pickdavid@gmail.com>
2011-10-18 23:53:47 -0500

    reverted jquery transitions because they looked terrible

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,19 +18,4 @@ $(document).ready(function() {
     tooltip   : 'Click to edit...',
   });
 
-  $("body").css("display", "none");
-
-  $("body").fadeIn(2000);
-
-  $("a.transition").click(function(event){
-      event.preventDefault();
-      linkLocation = this.href;
-      $("body").fadeOut(1000, redirectPage);
-  });
-
-  function redirectPage() {
-      window.location = linkLocation;
-  }
-  
-
 });
