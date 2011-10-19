9efc1d2b853295187beeab3494017fd67dd04c65
David Pick <pickdavid@gmail.com>
2011-10-18 23:48:07 -0500

    added transitions to the links

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -18,4 +18,19 @@ $(document).ready(function() {
     tooltip   : 'Click to edit...',
   });
 
+  $("body").css("display", "none");
+
+  $("body").fadeIn(2000);
+
+  $("a.transition").click(function(event){
+      event.preventDefault();
+      linkLocation = this.href;
+      $("body").fadeOut(1000, redirectPage);
+  });
+
+  function redirectPage() {
+      window.location = linkLocation;
+  }
+  
+
 });
